3804a7f66a36a152138df2f5361595349bf8b8ef
PranavBj2406 <pranavbj80@gmail.com>
2025-07-19 22:16:24 +0530

    pattern11 logic explained

--- patterns/Patterns.cpp
+++ patterns/Patterns.cpp
@@ -114,7 +114,7 @@ void pattern10(int num)
 }
 void pattern11()
 {
-    for(int i=0;i<5;i++)
+    for(int i=1;i<5;i++)
     {
         for(int j=1;j<i+1;j++)
         {
@@ -124,7 +124,7 @@ void pattern11()
         {
             cout<<" ";
         }
-        for(int j=1;j<i+1;j++)
+        for(int j=i;j>=1;j--) // logic to present no. in reverse
         {
             cout<<j;
         }
