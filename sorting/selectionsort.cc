81311af12fc08cd6e00a15e498cfebd57de15e5c
PranavBj2406 <pranavbj80@gmail.com>
2025-08-19 19:30:31 +0530

    selection sort completed

---
+++ sorting/selectionsort.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void swap(int &a,int &b) helper funtcion
+// {
+//     a=a+b;
+//     b=a-b;
+//     a=a+b;
+// }
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    // selection algo//
+    // find min in the array swap //
+    for(int i=0;i<n-2;i++)
+    {
+        int min=i;
+        for(int j=i;j<n;j++)  // jjob of the inner loop is to find the min element 
+        {
+            if(a[j]<a[min])
+            {
+                min=j;
+            }
+        }
+        swap(a[i],a[min]);  // used function from <algorithim> or you can write helper function
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i];
+    }
+
+return 0;
+}
