e036ed8669dd3cd23ff6128af62cec73ca7a6803
PranavBj2406 <pranavbj80@gmail.com>
2025-08-23 19:27:36 +0530

    rotation of elemnts understood

---
+++ array_series/p3.cc
@@ -0,0 +1,35 @@
+// rotation of the elements to the left
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> rotation(vector<int> &arr,int rotation_count)
+{
+    int k=0;
+    while(k<rotation_count) 
+    {
+        int temp=arr[0];
+        int n = arr.size();
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=arr[i+1];
+        }
+        arr[n-1]=temp;
+        return arr;
+    }
+}
+
+int main()
+{
+vector<int> arr;
+int k;
+cin>>k;// indicates the no. of rotations that must be performed
+arr={1,2,3,4,5};
+int n=arr.size();
+rotation(arr,k);
+for(int i=0;i<n;i++)
+{
+    cout<<arr[i]<<endl;
+}
+return 0;
+}
