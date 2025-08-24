96b8056d4b452a700252e5f9a78cea9f4a5f1c29
PranavBj2406 <pranavbj80@gmail.com>
2025-08-24 13:01:59 +0530

    Leet code problem 283 solved

---
+++ array_series/Leet Code/p3.cc
@@ -0,0 +1,54 @@
+// 283. Move Zeroes
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int i=0,j=-1;
+    for(int i=0;i<nums.size();i++)  
+    {
+        if(nums[i]==0)
+        {
+            j=i;
+            break;
+        }
+    }
+    if(j==-1)
+    {
+        return ;
+    }
+    for(i=j+1;i<nums.size();i++)
+    {
+        if(nums[i]!=0)
+        {
+            swap(nums[j],nums[i]);
+            j++;
+        }
+    } 
+    }
+};
