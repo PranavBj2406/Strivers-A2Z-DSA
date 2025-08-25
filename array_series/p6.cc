ac27221c4259dbb0b2d1b221166db0905c6b30dc
PranavBj2406 <pranavbj80@gmail.com>
2025-08-25 11:10:02 +0530

    intersection of 2 sorted arrays

--- array_series/p6.cc
+++ array_series/p6.cc
@@ -47,11 +47,48 @@ vector<int> union_array(vector<int> arr1,vector<int> arr2)
                 i++;
             }
     }
-    
+    return union1;
 
+}
+
+// intersection of two sorted arrays
+
+vector<int> intersection(vector<int> arr1,vector<int> arr2)
+{
+    int n1=arr1.size();
+    int n2=arr2.size();
+    int i=0;
+    int j=0;
+    vector<int> it;
+    while(i<n1 && j<n2)
+    {
+        if(arr1[i]>arr2[j])
+        {
+            j++;
+        }
+        if(arr1[i]<arr2[j])
+        {
+            i++;
+        }
+        if(arr1[i]==arr2[j])
+        {
+            it.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+    
+    for(int k=0;k<it.size();k++)
+    {
+        cout<<it[k];
+    }
+    return it;
 }
 
 int main()
 {
+    vector<int> arr1={1,1,2,2,4,5};
+    vector<int> arr2={1,2,2,3,6};
+    intersection(arr1,arr2);
 return 0;
 }
