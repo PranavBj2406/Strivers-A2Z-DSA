45225dd0339eead9176e19c78e490bdcc0811807
PranavBj2406 <pranavbj80@gmail.com>
2025-09-04 23:21:20 +0530

    opimal approach

--- array_series/p13.cc
+++ array_series/p13.cc
@@ -1,25 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-void arrange_arrays(vector<int> &nums)
+// brute force approach
+void arrange_arrays(vector<int> &nums) 
 {
-    vector<int> pos, neg;
-    for (int x : nums)
+    int n=nums.size();
+    vector<int> ans(n,0);
+    int posIndex=0;
+    int negIndex=1;
+    for(int i=0;i<nums.size();i++) // loop separate the +ve and -ve
     {
-        if (x > 0) pos.push_back(x);
-        else neg.push_back(x);
+        if(nums[i]>0)
+        {
+            ans[posIndex]==nums[i];
+            posIndex+=2;
+        }
+        else
+        {
+            ans[negIndex]==nums[i];
+            negIndex+=2;
+        }
     }
-
-    int i = 0, p = 0, n = 0;
-    while (p < pos.size() && n < neg.size()) {
-        nums[i++] = pos[p++];  // even index
-        nums[i++] = neg[n++];  // odd index
-    }
-
-    // if extras left (not always needed depending on problem statement)
-    while (p < pos.size()) nums[i++] = pos[p++];
-    while (n < neg.size()) nums[i++] = neg[n++];
 }
-
 
 int main()
 {
