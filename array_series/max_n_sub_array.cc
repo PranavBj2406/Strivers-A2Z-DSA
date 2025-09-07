0688a63a4fcbb88225e04da6f7799d29847f1bbf
PranavBj2406 <pranavbj80@gmail.com>
2025-09-07 13:03:56 +0530

    array -medium solved

---
+++ array_series/max_n_sub_array.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findAllSubArrayswithsumk(vector<int> &nums,int k)
+{
+    unordered_map<int,int> mpp;
+    mpp[0]=1;
+    int presum =0;
+    int cnt=0;
+    for(int i=0;i<nums.size();i++)
+    {
+        presum+=nums[i];
+        int remove = presum-k;
+        cnt+=mpp[remove];
+        mpp[presum]+=1;
+    }
+}
+int main()
+{
+return 0;
+}
