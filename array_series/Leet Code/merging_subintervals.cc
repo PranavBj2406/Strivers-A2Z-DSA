b48efc7958be168c4363409827480508d0e89c4d
PranavBj2406 <pranavbj80@gmail.com>
2025-09-24 23:30:03 +0530

    ..

--- array_series/Leet Code/merging_subintervals.cc
+++ array_series/Leet Code/merging_subintervals.cc
@@ -6,10 +6,13 @@ public:
         vector<vector<int>> ans;
         for(int i=0;i<n;i++)
         {
+            // Case 1: ans is empty OR current interval doesn't overlap
             if(ans.empty() || intervals[i][0]>ans.back()[1])
             {
                 ans.push_back(intervals[i]);
             }
+
+              // Case 2: There is an overlap → merge intervals v
             else
             {
                 ans.back()[1]=max(ans.back()[1],intervals[i][1]);
---
+++ array_series/merge_sorted_arr.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> mergearr(vector<int> arr1,vector<int> arr2)
+{
+    int n1=arr1.size();
+    int n2=arr2.size();
+    int Asize = n1+n2;
+    int start1=0,start2=0;
+    vector<int> ans;
+    while(left<m)
+
+    for(int i=0;i<n1;i++)
+    {
+        arr1[i]=ans[i];
+    }
+    for(int i=0;i<n2;i++)
+    {
+        arr2[i]=ans[i];
+    }
+}
+
+int main()
+{
+    
+return 0;
+}
