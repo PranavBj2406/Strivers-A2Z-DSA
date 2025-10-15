5cb65bc3df46c87f44748eebe2d0532f8d4b5c70
PranavBj2406 <pranavbj80@gmail.com>
2025-10-15 21:58:25 +0530

    max_product sub array solved:)

---
+++ array_series/max_product_subarray.cc
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int max_product_subarray(vector<int> nums) // this approach is for arr with odd negatives but no 0 itution based solution
+{
+    // note use long long to store prodcut
+    int n=nums.size();
+    int left=0;
+    int right=n-1;
+    int prefix=1;
+    int suffix=1;
+    int max_product=INT_MIN;
+    while(left<n && right>=0)
+    {
+        prefix=prefix*nums[left];
+        max_product=max(max_product,prefix);
+        suffix=suffix*nums[right];
+        max_product=max(max_product,suffix);
+        left++;
+        right--;
+
+        // consoder this logic if arr has 0,s in time_t
+
+        // prefix = (prefix == 0 ? nums[left] : prefix * nums[left]); // Reset on zero
+        // max_product = max(max_product, prefix);
+
+        // suffix = (suffix == 0 ? nums[right] : suffix * nums[right]);
+        // max_product = max(max_product, suffix)
+    }
+    return max_product;
+}
+int main()
+{
+return 0;
+}
