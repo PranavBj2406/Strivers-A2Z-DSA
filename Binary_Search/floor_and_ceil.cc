2fa49f79130bc8940c4f259f0acf0a39883cf25d
PranavBj2406 <pranavbj80@gmail.com>
2025-11-18 21:44:45 +0530

    floor_and_ceil && recursion

---
+++ Binary_Search/floor_and_ceil.cc
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+//floor means largest ele<=x while ceil means smallest ele >= x
+
+int main()
+{
+return 0;
+}
---
+++ recursion/p6.cc
@@ -0,0 +1,30 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void print_num(int num,int i=1)
+// {
+//    if(i>=num)
+//     {
+//         return ;
+//     }
+//     cout<<i<<endl;
+//     i++;
+//    print_num(num,i);
+// }
+
+
+// int main()
+// {
+//     int num;
+//     cin>>num;
+//     print_num(num);
+// return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+return 0;
+}
