d84c07ba0f36ebfa3f740679d2d55f7da8301619
PranavBj2406 <pranavbj80@gmail.com>
2026-02-04 23:30:57 +0530

    ..

---
+++ Stacks_Queues/min_stack.cc
@@ -0,0 +1,66 @@
+// brute method use hashing 
+
+class MinStack {
+public:
+        stack<pair<int,int>> pr;
+    MinStack() {
+    }
+    
+    void push(int val) {
+            if(pr.empty())
+            {
+                pr.push({val,val});
+            }
+            else
+            {
+                pr.push({val,min(val,pr.top().second)});
+            }
+    }
+    
+    void pop() {
+        pr.pop();
+    }
+    
+    int top() {
+        return pr.top().first;
+    }
+    
+    int getMin() {
+        return pr.top().second;
+    }
+};
+
+// optimal solution 
+class MinStack {
+public:
+    stack<int> value;
+    stack<int> minvalue;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(value.empty() && minvalue.empty())
+        {
+            value.push(val);
+            minvalue.push(val);
+        }
+        else
+        {
+        value.push(val);
+        minvalue.push(min(val,minvalue.top()));
+        }
+    }
+    
+    void pop() {
+        value.pop();
+        minvalue.pop();
+    }
+    
+    int top() {
+        return value.top();
+    }
+    
+    int getMin() {
+        return minvalue.top();
+    }
+};
