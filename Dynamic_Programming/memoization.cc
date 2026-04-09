e280a2a8e987ac28df09da018b9419bc550fe05f
PranavBj2406 <pranavbj80@gmail.com>
2026-04-09 13:48:49 +0530

    ..

---
+++ Dynamic_Programming/memoization.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+//memoization
+int fib(int n,vector<int> &arr)
+{
+    if(n<=1) return n;
+    else if(arr[n]!=-1) return arr[n];
+    else  return arr[n]=fib(n-2,arr)+fib(n-1,arr);
+}
+
+//tabulation
+int fib(int n)
+{
+   int prev2=0;
+   int prev=1;
+   for(int i=2;i<=n;i++)
+   {
+    int cur=prev2+prev;
+    prev2=prev;
+    prev=cur;
+   }
+   return prev;
+}
+
+
+
+int main()
+{
+    vector<int> arr(5,-1);
+    int A=fib(5,arr);
+    cout<<A;
+return 0;
+}
